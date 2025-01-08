0552fc7db681af3f2992c91dc27ec782a3f552d1
Pranjal1207sharma <pranjal1207sharma@gmail.com>
2025-01-08 23:34:15 +0530

    Simple Interest

---
+++ Simpleinterest.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Simpleinterest{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter Principle:"); 
+double p=sc.nextDouble();
+System.out.println("Enter rate:");
+double r=sc.nextDouble();
+System.out.println("Enter time:");
+double t=sc.nextDouble();
+double si = (p*r*t)/100;
+System.out.println("simple interest "+si);
+}
+}
