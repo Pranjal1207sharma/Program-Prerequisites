0583f7818332aff49cdf9d39fadac851bf2bfab1
Pranjal1207sharma <pranjal1207sharma@gmail.com>
2025-01-08 22:50:50 +0530

    Conversion

---
+++ Conversion.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Conversion{
+public static void main(String args[]) {
+Scanner sc=new Scanner(System.in);
+//Fahrenheit = (Celsius * 9/5) + 32.
+System.out.println("Enter temperature in Celsius:");
+double Celsius=sc.nextDouble();
+double Fahrenheit = (Celsius * 9/5) + 32;
+System.out.println("Fahrenheit = "+Fahrenheit);
+}
+}
